e76c8256741bdda54ed55e9a20b2d29c1e794beb
Jean Martins <jeanmrtns4@gmail.com>
2023-07-30 15:29:32 -0300

    feat: create base header component

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,12 @@
+import { styled } from "styled-components/native";
+
+export const Container = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Logo = styled.Image`
+  width: 46px;
+  height: 52px;
+`;
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,9 +1,11 @@
 import { Text } from "react-native";
 import { Container } from "./styles";
+import { Header } from "@components/Header";
 
 export const Groups = () => {
   return (
     <Container>
+      <Header />
       <Text>Hello Groups</Text>
     </Container>
   );
