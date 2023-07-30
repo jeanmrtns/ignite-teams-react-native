6938971ac9d91fd2879de09b13c34105874a1864
Jean Martins <jeanmrtns4@gmail.com>
2023-07-30 11:35:00 -0300

    feat: setup expo font

--- App.tsx
+++ App.tsx
@@ -1,8 +1,23 @@
 import { Groups } from "@screens/Groups";
 import theme from "@theme/index";
 import { ThemeProvider } from "styled-components/native";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import { ActivityIndicator } from "react-native";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <ActivityIndicator />;
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <Groups />
