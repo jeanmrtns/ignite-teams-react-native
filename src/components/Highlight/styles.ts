d47bb2c2e18f14216fd9eab87444516d454ffd40
Jean Martins <jeanmrtns4@gmail.com>
2023-08-13 11:48:08 -0300

    feat: create highlight component

---
+++ src/components/Highlight/styles.ts
@@ -0,0 +1,19 @@
+import { styled } from "styled-components/native";
+
+export const Container = styled.View`
+  margin-top: 32px;
+`;
+
+export const Title = styled.Text`
+  font-size: ${({ theme }) => theme.FONT_SIZE.XL}px;
+  font-family: ${({ theme }) => theme.FONT_FAMILY.BOLD};
+  color: ${({ theme }) => theme.COLORS.WHITE};
+  text-align: center;
+`;
+
+export const Subtitle = styled.Text`
+  font-size: ${({ theme }) => theme.FONT_SIZE.MD}px;
+  font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
+  color: ${({ theme }) => theme.COLORS.GRAY_300};
+  text-align: center;
+`;
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,12 +1,12 @@
-import { Text } from "react-native";
 import { Container } from "./styles";
 import { Header } from "@components/Header";
+import { Highlight } from "@components/Highlight";
 
 export const Groups = () => {
   return (
     <Container>
       <Header />
-      <Text>Hello Groups</Text>
+      <Highlight title="Turmas" subtitle="Jogue com sua turma" />
     </Container>
   );
 };
