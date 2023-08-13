8bc5313c0e762d1809cdf2b0e1207c983b9ce8c0
Jean Martins <jeanmrtns4@gmail.com>
2023-08-13 11:38:12 -0300

    feat: create back button for header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,18 @@
-import { Container, Logo } from "./styles";
+import { BackButton, BackIcon, Container, Logo } from "./styles";
 import logoImage from "@assets/logo.png";
 
-export function Header() {
+interface HeaderProps {
+  showBackButton?: boolean;
+}
+
+export function Header({ showBackButton = false }: HeaderProps) {
   return (
     <Container>
+      {showBackButton && (
+        <BackButton>
+          <BackIcon />
+        </BackButton>
+      )}
       <Logo source={logoImage} />
     </Container>
   );
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,12 +1,23 @@
+import { CaretLeft } from "phosphor-react-native";
 import { styled } from "styled-components/native";
 
 export const Container = styled.View`
   flex-direction: row;
   align-items: center;
   justify-content: center;
+  padding: 0 12px;
 `;
 
 export const Logo = styled.Image`
   width: 46px;
   height: 52px;
 `;
+
+export const BackButton = styled.TouchableOpacity`
+  flex: 1;
+`;
+
+export const BackIcon = styled(CaretLeft).attrs(({ theme }) => ({
+  size: 32,
+  color: theme.COLORS.WHITE,
+}))``;
